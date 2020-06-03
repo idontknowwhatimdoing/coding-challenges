d684a63b9df12587a0a8c935aad123480ce2a3c3
jeremy <jeyx.22@gmail.com>
2020-06-03 13:01:39 +0200

    so stuff

--- main.c
+++ main.c
@@ -3,7 +3,127 @@
 #include <stdlib.h>
 #include <string.h>
 
-int* breakingRecords(int scores_count, int* scores, int* result_count) {
+// Given an array of integers, find the sum of its elements.
+int simpleArraySum(int a[], unsigned int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) sum += a[i];
+
+    return sum;
+}
+
+// Calculate and print the sum of the elements in an array,keeping in mind that some of
+// those integers may be quite large.
+long aVeryBigSum(long a[], unsigned int size) {
+    long long int sum = 0;
+
+    for (int i = 0; i < size; i++) sum += a[i];
+
+    return sum;
+}
+
+// Alice and Bob each created one problem for HackerRank. A reviewer rates the two
+// challenges, awarding points on a scale from 1 to 100 for three categories: problem
+// clarity, originality, and difficulty.
+// We define the rating for Alice's challenge to be the triplet a = (a[0], a[1], a[2])
+// and the rating for Bob's challenge to be the triplet b = (b[0], b[1], b[2]).
+// Your task is to find their comparison points by comparing a[0] with b[0], a[1] with
+// b[1], and a[2] with b[2].
+// If a[i] > b[i], then Alice is awarded 1 point.
+// If a[i] < b[i], then Bob is awarded 1 point.
+// If a[i] = b[i], then neither person receives a point.
+int* compareTriplets(int a[], int b[], unsigned int size) {
+    int* result = malloc(sizeof(int) * 2);
+    result[0] = 0;
+    result[1] = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (a[i] > b[i])
+            result[0]++;
+        else if (a[i] < b[i])
+            result[1]++;
+    }
+
+    return result;
+}
+
+// Given a square matrix, calculate the absolute difference between the sums of its
+// diagonals.
+int diagonalDifference(int** a, unsigned int size) {
+    int sum_diag1 = 0, sum_diag2 = 0;
+
+    int j = size - 1;
+    for (int i = 0; i < size; i++) {
+        sum_diag1 += a[i][i];
+        sum_diag2 += a[i][j];
+        j--;
+    }
+
+    return abs(sum_diag1 - sum_diag2);
+}
+
+// Given an array of integers, calculate the fractions of its elements that are positive,
+// negative, and are zeros.
+void plusMinus(int a[], unsigned int size) {
+    int pos_count = 0, neg_count = 0, zero_count = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (a[i] > 0)
+            pos_count++;
+        else if (a[i] < 0)
+            neg_count++;
+        else
+            zero_count++;
+    }
+
+    printf("pos : %f\nneg : %f\nzero : %f", (float)pos_count / (float)size,
+           (float)neg_count / (float)size, (float)zero_count / (float)size);
+}
+
+// Print a staircase of size n using # symbols and spaces.
+void staircase(unsigned int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) printf(" ");
+        for (int j = 0; j < i; j++) printf("#");
+        printf("\n");
+    }
+}
+
+// You are in charge of the cake for your niece's birthday and have decided the cake will
+// have one candle for each year of her total age. When she blows out the candles, she’ll
+// only be able to blow out the tallest ones.
+unsigned int birthday_cake_candles(unsigned int a[], unsigned int size) {
+    unsigned int candles_count = 1, max = a[0];
+
+    for (int i = 1; i < size; i++) {
+        if (i > max)
+            max = i;
+        else if (i == max)
+            candles_count += 1;
+    }
+
+    return candles_count;
+}
+
+// HackerLand University has the following grading policy :
+// Every student receives a grade in the inclusive range from to 0 to 100.
+// Any grade less than 40 is a failing grade.
+// Sam is a professor at the university and likes to round each student's grade according
+// to these rules:
+// If the difference between the garde and the next multiple 5 of is less than 3, round up
+// to the next multiple of 5.
+// If the value of garde is less than 38, no rounding occurs as the result will still be a
+// failing grade.
+void gradingStudents(int grades[], unsigned int size) {
+    for (int i = 0; i < size; i++) {
+        if (grades[i] < 38)
+            continue;
+        else
+            grades[i] = grades[i] % 5 < 3 ? grades[i] : grades[i] + (5 - (grades[i] % 5));
+    }
+}
+
+int* breakingRecords(int scores[], unsigned int size) {
     int* records_count = malloc(sizeof(int) * 2);
     records_count[0] = 0;
     records_count[1] = 0;
@@ -11,7 +131,7 @@ int* breakingRecords(int scores_count, int* scores, int* result_count) {
     int most_points = scores[0];
     int least_points = scores[0];
 
-    for (int i = 0; i < scores_count; i++) {
+    for (int i = 0; i < size; i++) {
         if (scores[i] > most_points) {
             most_points = scores[i];
             records_count[0]++;
@@ -21,18 +141,17 @@ int* breakingRecords(int scores_count, int* scores, int* result_count) {
         }
     }
 
-    *result_count = 2;
     return records_count;
 }
 
-int sockMerchant(int n, int ar_count, int* ar) {
+int sockMerchant(int a[], unsigned int size, int n) {
     int totalPairs = 0, count = 1, nbElem = 0, paired = 0, pass = 0;
     int colorsPaired[1000];
 
-    for (int i = 0; i < ar_count - 1; i++) {
+    for (int i = 0; i < size - 1; i++) {
         // skip the indices of the colors already paired
         for (int k = 0; k < nbElem; k++) {
-            if (colorsPaired[k] == ar[i]) {
+            if (colorsPaired[k] == a[i]) {
                 pass = 1;
                 break;
             } else
@@ -41,15 +160,15 @@ int sockMerchant(int n, int ar_count, int* ar) {
 
         if (pass) continue;
 
-        for (int j = i + 1; j < ar_count; j++) {
-            if (ar[i] == ar[j]) {
+        for (int j = i + 1; j < size; j++) {
+            if (a[i] == a[j]) {
                 count++;
                 paired = 1;
             }
         }
 
         if (paired) {
-            colorsPaired[nbElem] = ar[i];
+            colorsPaired[nbElem] = a[i];
             nbElem++;
         }
 
@@ -115,12 +234,12 @@ int countingValleys(int n, char* s, unsigned int size) {
     return countValleys;
 }
 
-int divisibleSumPairs(int n, int k, int ar[]) {
+int divisibleSumPairs(int a[], unsigned int size, int k) {
     int count = 0;
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if ((ar[i] + ar[j]) % k == 0) count++;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if ((a[i] + a[j]) % k == 0) count++;
         }
     }
 
@@ -133,7 +252,7 @@ int divisibleSumPairs(int n, int k, int ar[]) {
 // For example, if your array is, a = [1, 1, 2, 2, 4, 4, 5, 5, 5]
 // you can create two subarrays meeting the criterion : [1, 1, 2, 2] and [4, 4, 5, 5, 5].
 // The maximum length subarray has 5 elements.
-int pickingNumbers(int size, int a[]) {
+int pickingNumbers(int a[], int size) {
     int max = 0;
 
     for (int i = 0; i < size; i++) {
@@ -165,7 +284,7 @@ int pickingNumbers(int size, int a[]) {
     return max;
 }
 
-int picking_numbers_opti(unsigned int size, unsigned int a[]) {
+int picking_numbers_opti(unsigned int a[], unsigned int size) {
     int count, prev = -1, max = 0;
 
     // sort(a);
@@ -224,12 +343,28 @@ int reverse(int x) {
 // You will be given 3x3 a matrix s of integers in the inclusive range [1, 9].
 // We can convert any digit a to any other digit b in the range at cost of |a - b|.
 // Given, s convert it into a magic square at minimal cost.
-int formingMagicSquare(unsigned int rows, unsigned int cols, unsigned int** s) {}
+int formingMagicSquare(unsigned int rows, unsigned int cols, unsigned int** s) {
+    // TODO
+}
+
+void display(int a[], unsigned int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", a[i]);
+    }
+    printf("\n");
+}
 
 int main() {
-    int test[4] = {2, 7, 11, 15};
+    // int test1[3] = {2, 7, 11};
+    // int test2[3] = {2, 7, 11};
+    // int* result = compareTriplets(test1, test2, 3);
+    // display(result, 2);
 
-    two_sum(test, 4, 17);
+    // staircase(6);
+
+    int test[4] = {73, 67, 38, 33};
+    gradingStudents(test, 4);
+    display(test, 4);
 
     return 0;
 }
--- main.rs
+++ main.rs
@@ -1,16 +1,15 @@
-fn bon_appetit(bill: Vec<i32>, k: i32, b: i32) {
-	let mut total_sum = 0;
-	let mut sum_k = 0;
+// Given five positive integers, find the minimum and maximum values that can be
+// calculated summing exactly four of the five integers
+fn min_max_sum(a: Vec<i32>) {
+    let mut min = 0, max = 0;
 
-	for i in bill {
-		total_sum += i;
-		if i != k { sum_k += i; }	
-	}
+    a.sort_unstable();
 
-	if sum_k / 2 == b { println!("bon app"); }
-	else { println!("{}", (total_sum / 2) - (sum_k / 2)); }
+    for i in 0..a.len()-1 { min += a[i]; } 
+    for i in 1..a.len() { max += a[i]; } 
 }
 
+// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
 fn time_conversion(s: &str) -> String {
 	if s.ends_with("AM") {
 		if s.starts_with("12") {
@@ -33,17 +32,25 @@ fn time_conversion(s: &str) -> String {
 	}
 }
 
-fn reverse(mut x: i32) -> i32 {
-	let mut rev = 0;
+fn bon_appetit(bill: Vec<i32>, k: i32, b: i32) {
+	let mut total_sum = 0;
+	let mut sum_k = 0;
 
-	while x != 0 {
-		let pop = x % 10; // get the last digit of the number
-		x /= 10; // go to the next digit
-		rev = rev * 10 + pop; // add the digit to the begining of the new number
+	for i in bill {
+		total_sum += i;
+		if i != k { sum_k += i; }	
 	}
 
-	rev
+	if sum_k / 2 == b { println!("bon app"); }
+	else { println!("{}", (total_sum / 2) - (sum_k / 2)); }
 }
+
+// When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis.
+// A negative value of d means the fruit fell d units to the tree's left
+// and a positive value of means it falls units to the tree's right.
+// Given the value of d for m apples and n oranges
+// determine how many apples and oranges will be in the inclusive range [s, t].
+fn apples_oranges(apples: Vec<i32>, oranges: Vec<i32>, s: i32, t: i32, a: i32, b: i32) {}
 
 fn main() {
 	let arg = "07:05:45PM";
